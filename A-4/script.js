3a1575dfb102c02b192989e781ae61bc7c43bf32
sudarshan sangle <darshsangle9456@gmail.com>
2025-02-22 23:54:45 +0530

    this check error while push code

---
+++ A-4/script.js
@@ -0,0 +1,20 @@
+function validateForm(){
+
+    let name = document.getElementById("name").value;
+    let mob = document.getElementById("mobile").value;
+    let age = document.getElementById("age").value;
+
+
+
+    if(name.trim() ===""){
+        alert("please enter valid name")
+    }
+
+    if(mob.length != 10){
+               alert("enter valid number");
+    }
+    if(age < 18){
+        console.log("yotr not eligible ok")
+        alert("you not eligible");
+    }
+}
